110241469f9c45107338c3a97164d766e0724c97
R. Tyler Croy <tyler@monkeypox.org>
2014-06-03 23:01:52 -0700

    Add TODO for extending Elementary::Future

--- lib/elementary/future.rb
+++ lib/elementary/future.rb
@@ -3,5 +3,17 @@ require 'concurrent'
 
 module Elementary
   class Future < Concurrent::Future
+    # TODO: Implement method_missing such that any method not defined on
+    # Concurrent::Future will cause the future to be resolved and the method to
+    # be called on the resulting value.
+    #
+    # This would make things like:
+    #
+    #   results = c.rpc.search(query)
+    #   # sleep maxint
+    #   results.each do |result|
+    #     # work
+    #   end
+    # 
   end
 end
