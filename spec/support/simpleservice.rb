fdcbb79a92acefedff0a3db2ed481d47ddc72125
R. Tyler Croy <tyler@monkeypox.org>
2014-06-03 20:58:47 -0700

    Add an actual service definition that does "work" for the tests

---
+++ spec/support/simpleservice.rb
@@ -0,0 +1,15 @@
+require './spec/support/simpleservice.pb'
+
+module Elementary
+  module Rspec
+    class Simple
+      def echo
+        puts "ECHOING #{request.inspect}"
+        sleep 3
+
+        respond_with(String.new(:data => request.data))
+      end
+    end
+  end
+end
+
