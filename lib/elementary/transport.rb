3126f796b5f0ee6f8990e208afa5322c9006a7d2
R. Tyler Croy <tyler@monkeypox.org>
2014-06-16 18:17:50 -0700

    Refactor the Elementary::Transport::HTTP into the more conventional location

--- lib/elementary/transport.rb
+++ lib/elementary/transport.rb
@@ -1,46 +1,6 @@
-require 'rubygems'
-require 'cgi'
-require 'faraday'
-require 'socket'
-
-require 'elementary/future'
-
 module Elementary
   module Transport
-    class HTTP
-      def initialize(hosts)
-        @hosts = hosts
-      end
-
-      def call(service, rpc_method, *params)
-        # MIDDLEWARE STACK TERMINATOR
-        response = client.post do |h|
-          path = "/#{CGI.escape(service.name)}/#{rpc_method.method}"
-          h.url(path)
-          h.body = params[0].encode
-        end
-
-        # XXX: Need to raise on failures?
-        return rpc_method[:response_type].decode(response.body)
-      end
-
-      private
-
-      def host_url
-        # XXX: need to support a full collection of hosts similar to
-        # elasticsearch-ruby
-        host = @hosts.first
-        return "http://#{host[:host]}:#{host[:port]}/"
-      end
-
-      def client
-        # XXX: client caching, duh
-        Faraday.new(:url => host_url) do |f|
-          f.response :logger
-          f.adapter Faraday.default_adapter
-        end
-      end
-    end
   end
 end
 
+require 'elementary/transport/http'
